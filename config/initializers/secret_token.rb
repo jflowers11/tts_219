e4f43c8ecd8e127dadbe2b1461850edc49c91c3a
J Flowers <jaquetaflowers@gmail.com>
2014-02-21 13:36:21 -0500

    first load

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tts219::Application.config.secret_key_base = '60c1287153d7b9a4eaf72a975a35ce0fd76747596db27d6753dfb6afa8d8fd1731d9403ca9abcdc2f6210b19637aa2215166b35b46c58e322727a4ff870efe85'
